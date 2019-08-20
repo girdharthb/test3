f21a9830f943b3e158f296198d88eabd597edcab
girdharthb <girdhar@thb.co.in>
2019-08-20 13:28:28 +0530

    ty

---
+++ diary.R
@@ -0,0 +1,16 @@
+addy<-function(){
+nm <- readline(prompt="Enter name: ")
+dincharya <- readline(prompt="How good was your day on 1-10: ")
+Dudeness <- readline(prompt="How non-needy you are today on 1-10: ")
+
+koimila <- readline(prompt="Did you meet someone new: ")
+if(koimila=='1'){
+konmila <- readline(prompt="What is her name: ")
+}
+else{konmila<-'N'}
+}
+
+
+
+
+
